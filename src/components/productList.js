36c30e19b6b978c53e939e57447c568b6ab8ea84
vishnuerema <007vishnue@gmail.com>
2020-07-04 22:37:40 +0530

    sec

--- src/components/productList.js
+++ src/components/productList.js
@@ -13,24 +13,21 @@ export default class ProductList extends Component {
       super(props);
       this.state = {
         data: [],
-        count:0
+        count:[0]
       }
     }
 
-    increment = (e) =>{
-        this.setState({count: this.state.count + 1});
-        // var iniArr=[];
-        // iniArr.push(this.state.count)
-        // var jsonInarra = JSON.stringify(iniArr)
-        // localStorage.setItem("product_count" , jsonInarra);
-        // console.log(JSON.parse(localStorage['iniArr']))
+    increment(index){
+        let newCount = this.state.count;
+        newCount[index] = this.state.count[index] ? this.state.count[index] + 1 : 1;
+        this.setState({count: newCount});
+    }
+    decrement(index){
+        let newCount = this.state.count;
+        newCount[index] = this.state.count[index] ? this.state.count[index] - 1 : 0;
+            this.setState({count: newCount});
     }
 
-    decrement = (e) =>{
-        if(this.state.count > 0){
-            this.setState({count: this.state.count - 1});
-        }
-    } 
     componentDidMount () {
         fetch("https://www.mocky.io/v2/5dfccffc310000efc8d2c1ad")
         .then(res => res.json())
@@ -92,7 +89,7 @@ export default class ProductList extends Component {
                                         <h6 >{items.dish_name}</h6>
                                         <div className="amount_list"><span className="amount_list1">{items.dish_currency} {items.dish_price}</span> <span className="calorie_list">{items.dish_calories} Calories</span></div>
                                 <p className="description">{items.dish_description}</p>
-                                <div className="add_remove"><button onClick ={this.increment} type="button" className="btn_cmnn add_btn">+</button> {this.state.count }<button onClick ={this.decrement} type="button" className="btn_cmnn remov_btn">-</button></div>
+                                <div className="add_remove"><button onClick ={()=>this.increment(i)} type="button" className="btn_cmnn add_btn">+</button> {this.state.count[i] || 0 }<button onClick ={()=>this.decrement(i)} type="button" className="btn_cmnn remov_btn">-</button></div>
                                 <div className="customization">{items.dish_Availability ? 'Customization Available' : '' }</div>
                                     </div>
                                     <div className="food_list_img">
